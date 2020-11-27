1b068eb8e67ec4324360b608ebbfee663f53dffc
周子凱 <andychou@zhouzikaideMacBook-Air.local>
2020-11-27 16:34:59 +0800

    feat: 019_Remove_Nth_Node_From_End_of_List.

---
+++ Classic/019_Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,42 @@
+'''
+Solution: Linked List & Two Pointers
+
+dummy
+-1    ->    1    ->    2    ->    3    ->    4    ->    5    ->    None
+fase
+slow
+-------
+讓 fast 先走 2 單位
+-1    ->    1    ->    2    ->    3    ->    4    ->    5    ->    None
+slow -------2------- fast
+-------
+直到fast.next = None 時, slow都維持兩單位的差
+-1    ->    1    ->    2    ->    3    ->    4    ->    5    ->    None
+                                 slow -------2------- fast
+-------
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head, n):
+    dummy = ListNode(-1)
+    dummy.next = head
+
+    fast = dummy
+    slow = dummy
+
+    # 讓 fast 先走 n 單位
+    for _ in range(n):
+        fast = fast.next
+    
+    while fast.next != None:
+        fast = fast.next
+        slow = slow.next
+    
+    slow.next = slow.next.next
+    return dummy.next
+
+print(removeNthFromEnd([1,2,3,4,5], 2)) # [1,2,3,5]
