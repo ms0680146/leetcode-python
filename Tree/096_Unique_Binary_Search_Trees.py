eba5cb6291dd5517e458b26c7f0d06d32c10f678
andychou <andychou@25sprout.com>
2021-03-05 15:44:06 +0800

    fix: 096_Unique_Binary_Search_Trees Button Up DFS

--- Tree/096_Unique_Binary_Search_Trees.py
+++ Tree/096_Unique_Binary_Search_Trees.py
@@ -6,16 +6,35 @@ Input: numTrees(0), Output: 1
 Input: numTrees(1), Output: 1
 Input: numTrees(2) (use 1,2 as root), Output: numTrees(0)*numTrees(1) + numTrees(1)*numTrees(0)=2
 Input: numTrees(3) (use 1,2,3 as root), Output: numTrees(0)*numTrees(2) + numTrees(1)*numTrees(1) + numTrees(2)*numTrees(0)
+
+Button Up DFS
+- Base Case
+- 向子問題要答案(return value)
+- 利用子問題的答案構建當前問題的答案
+- 返回答案給父問題
+
+O(n**2)
 '''
 
-def numTrees(n):
-    numTree = {0:1, 1:1}
-    for node in range(2, n+1): # n=2, range(2,3)=2
-        total = 0
-        for root in range(1, node+1): # node=2, range(1,3)=1,2
-            left = numTree[root-1]
-            right = numTree[node-root]
-            total += left * right
-        numTree[node] = total  
+class Solution:
+    def numTrees(self, n):
+        self.memo = [None for _ in range(n+1)]
+        return self.dfs(n)
     
-    return numTree[n]
+    def dfs(self, n):
+        if n <= 1:
+            return 1
+        
+        if self.memo[n] != None:
+            return self.memo[n]
+        
+        result = 0
+        for i in range(1, n+1): #1,2,3
+            left = self.dfs(i-1)
+            right = self.dfs(n-i)
+            result += left * right
+        
+        self.memo[n] = result
+        
+        return result
+
