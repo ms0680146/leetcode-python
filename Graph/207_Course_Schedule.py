11f877dc8ff7ce661e6d6408114ea61581576cb4
andychou <ms0680146@gmail.com>
2023-10-11 16:20:41 +0800

    add graph

--- Graph/207_Course_Schedule.py
+++ Graph/207_Course_Schedule.py
@@ -1,4 +1,25 @@
-class Solution:
+'''
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+'''
+import collections
+
+class SolutionBFS:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         indegree = [0] * numCourses
         graph = { j:[] for j in range(numCourses) }
@@ -23,4 +44,39 @@ class Solution:
 
         for i in range(numCourses):
             if indegree[i]!= 0: return False
+        return True
+
+'''
+prerequisites = [[0,1],[0,2],[1,3],[1,4],[3,4]]
+preMap = {
+ crs: pre
+ 0: [1,2],
+ 1: [3,4],
+ 2: [],
+ 3: [4],
+ 4: []
+}
+'''
+class SolutionDFS:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
+        visiting = set()
+        def dfs(crs):
+            if crs in visiting: return False
+            if preMap[crs] == []: return True
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
         return True
--- Tree/094_Binary_Tree_Inorder_Traversal.py
+++ Tree/094_Binary_Tree_Inorder_Traversal.py
@@ -5,7 +5,11 @@ Input: root = [1,null,2,3]
 Output: [1,3,2]
 
 Inorder: left -> node -> right
-先走左邊並將node存進stack當中，當左邊都走完時，從stack pop出最上面的node，存進result當中，在開始走右邊。
+先走左邊並將 node 存進 stack 當中
+當左邊都走完時
+從 stack pop 出最上面的 node
+存進 result 當中 
+在開始走右邊
 '''
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -34,17 +38,14 @@ Top Down DFS Inorder Traversal
 '''
 class Solution:
     def inorderTraversal(self, root):
-        result = []
-        if root == None:
-            return result
+        res = []
+
+        def dfs(root):
+            if root == None:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
         
-        self.dfs(root, result)
-        return result
-        
-    def dfs(self, node, result):
-        if node == None:
-            return
-        
-        self.dfs(node.left, result)
-        result.append(node.val)
-        self.dfs(node.right, result)
+        dfs(root)
+        return res
